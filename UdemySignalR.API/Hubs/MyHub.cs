a39e7f82ae36783bcafdc1e1aa87d83205fafcb5
BurakAyan97 <burakayan97@outlook.com>
2023-07-24 22:59:15 +0300

    updated

--- UdemySignalR.API/Hubs/MyHub.cs
+++ UdemySignalR.API/Hubs/MyHub.cs
@@ -7,15 +7,24 @@ namespace UdemySignalR.API.Hubs
 {
     public class MyHub : Hub
     {
+        public static int TeamCount { get; set; } = 7;
         private static int ClientCount { get; set; } = 0;
         public static List<string> Names { get; set; } = new List<string>();//API açık olduğu sürece clientlar belli olacak.Database kullanmadığımız için burda tutuyoruz eğitim için.
 
         //Clientların çağıracağı methodları tanımlıyoruz
         public async Task SendName(string name)
         {
-            Names.Add(name);
-            //Clientlardaki (string yazılan isimdeki method) çalışacak ve message değerini de aynı zamanda gönderecek.
-            await Clients.All.SendAsync("ReceiveName", name);
+            if (Names.Count >= TeamCount)
+            {
+                //Caller propertysi sadece tek bir kişiye mesaj gönderilmesini sağlar
+                await Clients.Caller.SendAsync("Error", $"Takım en fazla {TeamCount} kişi olabilir.");
+            }
+            else
+            {
+                Names.Add(name);
+                //Clientlardaki (string yazılan isimdeki method) çalışacak ve message değerini de aynı zamanda gönderecek.
+                await Clients.All.SendAsync("ReceiveName", name);
+            }
         }
 
         public async Task GetNames()
