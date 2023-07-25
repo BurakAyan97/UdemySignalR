2116615e977954c4db927447230ff19ba7c7b0c2
BurakAyan97 <burakayan97@outlook.com>
2023-07-25 22:26:50 +0300

    new Project added

---
+++ CovidChart.API/Models/Covid.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CovidChart.API.Models
+{
+    public enum ECity
+    {
+        Istanbul = 1,
+        Ankara,
+        Izmir,
+        Konya,
+        Antalya
+    }
+    public class Covid
+    {
+        public int Id { get; set; }
+        public ECity ECity { get; set; }
+        public int Count { get; set; }
+        public DateTime CovidDate { get; set; }
+    }
+}
